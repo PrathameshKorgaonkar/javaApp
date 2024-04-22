2f6f073e637146a315dccecfa156bd41b10b9da5
Prathamesh Prakash Korgaonkar <141243426+PrathameshKorgaonkar@users.noreply.github.com>
2024-04-22 16:55:25 +0530

    Update AppTest.java

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -1,8 +1,7 @@
 package com.mycompany.app;
 
 import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest
 {
@@ -19,4 +18,28 @@ public class AppTest
         App app = new App();
         assertEquals("Hello World!", app.getMessage());
     }
+
+    @Test
+    public void testAppMessageChange() {
+        App app = new App();
+        String newMessage = "New Message!";
+        app.setMessage(newMessage);
+        assertEquals(newMessage, app.getMessage());
+    }
+
+    @Test
+    public void testAppMessageNotNull() {
+        App app = new App();
+        assertNotNull(app.getMessage());
+    }
+    
+    @Test
+    public void testAppMessageLength() {
+        App app = new App();
+        assertEquals(12, app.getMessage().length());
+    }
 }
+
+
+    
+
